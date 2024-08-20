9e1123f5d1e7f4428c44dfeb71f963d8a8242355
pragati-29 <131236738+pragati-29@users.noreply.github.com>
2024-08-20 14:32:58 +0530

    Delete Common_Variants directory

--- Common_Variants/code_4_zipping.sh
+++
@@ -1,4 +0,0 @@
-
-for file in /media/bioinfoa/bioinfo2/Pragati/Common_Variant/Try/anno/*; do
-    bgzip "$file"
-done
--- Common_Variants/indexing-code.sh
+++
@@ -1,3 +0,0 @@
-for file in /media/bioinfoa/bioinfo2/Pragati/Common_Variant/Try/somatic/*; do
-    bcftools index  -tf "$file"
-done
